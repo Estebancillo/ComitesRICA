6a7bafb0a65690086932778a1328a9b3efc34b28
SaulSandovalM <sauldevelop@gmail.com>
2018-07-09 17:36:52 -0500

    Pull firebase

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createStackNavigator} from 'react-navigation';
-
+//Componentes
 import welcome from './src/components/welcome/welcome';
 import Home from './src/components/Home/Home';
 import groups from './src/components/groups/groups';
@@ -12,35 +12,23 @@ import Miercoles from './src/components/consumo/miercoles';
 
 const Routes = createStackNavigator({
   Miercoles:Miercoles,
-Login:Login,
-
-consumo:consumo,
-Map: Map,
-
-Register: Register,
-Home: Home,
-welcome: welcome,
-groups: groups,
-
-
-
-
+  Login:Login,
+  consumo:consumo,
+  Map: Map,
+  Register: Register,
+  Home: Home,
+  welcome: welcome,
+  groups: groups,
 },
-
 {
-
   initialRoute: 'Miercoles'
-});
+}
+);
 
 export default class App extends React.Component{
-
   render(){
     return(
       <Routes />
-
-      );
-
-
-
+    );
   }
 }
